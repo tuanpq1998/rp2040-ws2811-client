6cd60f7667b27c7ca7e3e24498c33e2a57f3bca3
tuanpq1998 <tuanquang123321@gmail.com>
2025-12-03 00:08:22 +0700

    config and custom dir

--- main.hpp
+++ main.hpp
@@ -2,8 +2,8 @@
 
 #include <stdio.h>
 
-const uint DATA_IN_PIN = 13;
-const uint DATA_OUT_PIN = 14;
+const uint DATA_IN_PIN = 20;
+const uint DATA_OUT_PIN = 16;
 // #define SIDESET_PIN = 8;
 
 const uint NUM_LEDS_TO_EMULATE = 1;
