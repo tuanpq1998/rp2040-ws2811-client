9bf2f28ff2ab25dc88ef9377c1fce09863d317b1
tuanpq1998 <tuanquang123321@gmail.com>
2025-12-04 00:45:17 +0700

    ...

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
+#include <cstdio>          // <-- required for printf
 #include "pico/stdlib.h"
+
 int main() {
     stdio_init_all();
     sleep_ms(1500);
-    for (int i=0;; ++i) {
-        printf("USB WORKS %d\n", i);
+
+    while (true) {
+        printf("USB WORKS\n");
+
         gpio_set_dir(25, GPIO_OUT);
-        gpio_put(25, i & 1);
-        sleep_ms(1000);
+        gpio_put(25, 1);
+        sleep_ms(500);
+        gpio_put(25, 0);
+        sleep_ms(500);
     }
 }
