e929c9f21d7bb1a5bfd293040aa04847841a87e5
tuanpq1998 <tuanquang123321@gmail.com>
2025-12-04 10:47:21 +0700

    ...

--- main.cpp
+++ main.cpp
@@ -1,18 +1,15 @@
-// main.cpp — minimal USB + blink test (C++)
-#include <cstdio>
 #include "pico/stdlib.h"
 #include "tusb.h"
 
-
 int main() {
     stdio_init_all();
-    sleep_ms(2000);     // allow time for USB CDC to connect
+    sleep_ms(2000);  // allow USB to enumerate
 
     gpio_init(25);
     gpio_set_dir(25, GPIO_OUT);
 
     while (true) {
-        tud_task();     // IMPORTANT! allow USB CDC to process
+        tud_task();  // allow USB CDC to run
 
         printf("USB WORKS\n");
 
