cfd33196e5e27d78050a1d508230c7a5085ed7bb
tuanpq1998 <tuanquang123321@gmail.com>
2025-12-04 11:10:20 +0700

    Revert "..."
    This reverts commit 9bf2f28ff2ab25dc88ef9377c1fce09863d317b1.

--- main.cpp
+++ main.cpp
@@ -1,17 +1,11 @@
-#include <cstdio>          // <-- required for printf
 #include "pico/stdlib.h"
-
 int main() {
     stdio_init_all();
     sleep_ms(1500);
-
-    while (true) {
-        printf("USB WORKS\n");
-
+    for (int i=0;; ++i) {
+        printf("USB WORKS %d\n", i);
         gpio_set_dir(25, GPIO_OUT);
-        gpio_put(25, 1);
-        sleep_ms(500);
-        gpio_put(25, 0);
-        sleep_ms(500);
+        gpio_put(25, i & 1);
+        sleep_ms(1000);
     }
 }
