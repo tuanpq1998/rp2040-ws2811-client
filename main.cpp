00941b6e4739d85c2a0035d7995e4adff11e8683
tuanpq1998 <tuanquang123321@gmail.com>
2025-12-10 10:51:55 +0700

    edit

--- main.cpp
+++ main.cpp
@@ -128,8 +128,8 @@ int main() {
             fflush(stdout);
             sleep_ms(10);  
             
-            sleep_run_from_rosc();
-            sleep_goto_dormant_until_level_high(LED_PWR_PIN);
+            // sleep_run_from_rosc();
+            sleep_goto_dormant_until_edge_high(LED_PWR_PIN);
             
             sleep_power_up();
 
