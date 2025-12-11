e70deabc0f83a2546e5b01db6d1f2b3a06794ed5
tuanpq1998 <tuanquang123321@gmail.com>
2025-12-11 21:17:27 +0700

    add flag and use

--- main.cpp
+++ main.cpp
@@ -10,7 +10,6 @@
 #include "hardware/pio.h"
 #include "hardware/clocks.h"
 #include "hardware/rosc.h"
-#include "hardware/timer.h"
 
 #include "ws2812.pio.h"
 
@@ -96,7 +95,7 @@ inline void correct_color_fast(uint8_t r, uint8_t g, uint8_t b,
     b2 = clamp8(bf);
 }
 
-int64_t push_fan_switch(alarm_id_t id, void *user_data) {
+int64_t push_fan_switch() {
     gpio_put(FAN_SWITCH_PIN, 1);
     sleep_ms(200);
     gpio_put(FAN_SWITCH_PIN, 0);
